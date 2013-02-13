9e600f27beab41c1b423e30005bdba44ad902d88
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2013-02-13 12:12:45 -0500

    Reindent file

--- src/main/java/org/textensor/util/inst.java
+++ src/main/java/org/textensor/util/inst.java
@@ -11,42 +11,42 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 public abstract class inst {
-        public static <T> LinkedList<T> newLinkedList() {
-                return new LinkedList<T>();
-        }
+    public static <T> LinkedList<T> newLinkedList() {
+        return new LinkedList<T>();
+    }
 
-        public static <T> LinkedList<T> newLinkedList(Collection<? extends T> c) {
-                return new LinkedList<T>(c);
-        }
+    public static <T> LinkedList<T> newLinkedList(Collection<? extends T> c) {
+        return new LinkedList<T>(c);
+    }
 
-        public static <T> LinkedList<T> newLinkedList(Iterator<? extends T> it) {
-                LinkedList<T> list = new LinkedList<T>();
-                while(it.hasNext())
-                        list.add(it.next());
-                return list;
-        }
+    public static <T> LinkedList<T> newLinkedList(Iterator<? extends T> it) {
+        LinkedList<T> list = new LinkedList<T>();
+        while(it.hasNext())
+            list.add(it.next());
+        return list;
+    }
 
-        public static <T> ArrayList<T> newArrayList() {
-                return new ArrayList<T>();
-        }
+    public static <T> ArrayList<T> newArrayList() {
+        return new ArrayList<T>();
+    }
 
-        public static <T> ArrayList<T> newArrayList(int size) {
-                return new ArrayList<T>(size);
-        }
+    public static <T> ArrayList<T> newArrayList(int size) {
+        return new ArrayList<T>(size);
+    }
 
-        public static <K,V> HashMap<K,V> newHashMap() {
-                return new HashMap<K,V>();
-        }
+    public static <K,V> HashMap<K,V> newHashMap() {
+        return new HashMap<K,V>();
+    }
 
-        public static <K,V> LinkedHashMap<K,V> newLinkedHashMap() {
-                return new LinkedHashMap<K,V>();
-        }
+    public static <K,V> LinkedHashMap<K,V> newLinkedHashMap() {
+        return new LinkedHashMap<K,V>();
+    }
 
-        public static <K,V> TreeMap<K,V> newTreeMap() {
-                return new TreeMap<K,V>();
-        }
+    public static <K,V> TreeMap<K,V> newTreeMap() {
+        return new TreeMap<K,V>();
+    }
 
-        public static <T> TreeSet<T> newTreeSet() {
-                return new TreeSet<T>();
-        }
+    public static <T> TreeSet<T> newTreeSet() {
+        return new TreeSet<T>();
+    }
 }
