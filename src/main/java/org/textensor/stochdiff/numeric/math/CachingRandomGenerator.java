325d48b5ed28a90f9efdfa7dc555b8ef98382124
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2013-02-15 11:45:58 -0500

    Use a big queue for the CachingRandomGenerator
    This version actually dies after exhausting heap space.
    But it looks nice :).

--- src/main/java/org/textensor/stochdiff/numeric/math/CachingRandomGenerator.java
+++ src/main/java/org/textensor/stochdiff/numeric/math/CachingRandomGenerator.java
@@ -1,6 +1,8 @@
 package org.textensor.stochdiff.numeric.math;
 
 import java.util.concurrent.BlockingQueue;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import org.textensor.util.inst;
 
@@ -8,8 +10,9 @@ import org.textensor.util.inst;
  * Provide caching for a real random generator.
  */
 public class CachingRandomGenerator extends MersenneDerived {
+    static final Logger log = LogManager.getLogger(CachingRandomGenerator.class);
 
-    public static final int CAPACITY = 100;
+    public static final int CAPACITY = 1073741824;
 
     protected final BlockingQueue<Float> random = inst.newArrayBlockingQueue(CAPACITY);
 
@@ -19,10 +22,15 @@ public class CachingRandomGenerator extends MersenneDerived {
         Runnable f = new Runnable() {
                 @Override
                 public void run() {
+                    int i = 0;
                     try {
-                        while(true)
+                        while(true) {
                             random.put(gen.random());
+                            if(i++ % 1048576 == 0)
+                                log.info("random queue size is {}", random.size());
+                        }
                     } catch(InterruptedException e) {
+                        log.info("filler interrupted");
                     }
                 }
             };
