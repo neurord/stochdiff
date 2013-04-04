be6c6983e38301ed4d01ac2b5a6e9b9163a435a8
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2013-04-04 11:38:13 -0400

    tests: repeat StepGen tests a few times and improve output

--- src/test/java/org/textensor/stochdiff/TestStepGenerator.java
+++ src/test/java/org/textensor/stochdiff/TestStepGenerator.java
@@ -1,5 +1,7 @@
 package org.textensor.stochdiff;
 
+import java.util.Random;
+
 import org.textensor.report.E;
 import org.textensor.stochdiff.numeric.math.MersenneTwister;
 import org.textensor.stochdiff.numeric.stochastic.StepGenerator;
@@ -17,44 +19,46 @@ public class TestStepGenerator {
 
     @DataProvider
     public Object[][] means() {
+        Random r = new Random();
         return new Object[][] {
-            new Object[] { 1.e-7},
-            new Object[] { 1.e-6},
-            new Object[] { 1.e-5},
-            new Object[] { 1.e-4},
-            new Object[] { 1.e-3},
-            new Object[] { 1.e-2},
-            new Object[] { 1.e-1},
-            new Object[] { 0.5},
-            new Object[] { 0.9},
-            new Object[] { 1.1},
-            new Object[] { 3.},
-            new Object[] { 10},
-            new Object[] { 30},
-            new Object[] { 100},
+            new Object[] { 1.e-7, r.nextLong()},
+            new Object[] { 1.e-6, r.nextLong()},
+            new Object[] { 1.e-5, r.nextLong()},
+            new Object[] { 1.e-4, r.nextLong()},
+            new Object[] { 1.e-3, r.nextLong()},
+            new Object[] { 1.e-2, r.nextLong()},
+            new Object[] { 1.e-1, r.nextLong()},
+            new Object[] { 0.5, r.nextLong()},
+            new Object[] { 0.9, r.nextLong()},
+            new Object[] { 1.1, r.nextLong()},
+            new Object[] { 3., r.nextLong()},
+            new Object[] { 10, r.nextLong()},
+            new Object[] { 30, r.nextLong()},
+            new Object[] { 100, r.nextLong()},
+            new Object[] { 300, r.nextLong()},
         };
     };
 
     @Test(dataProvider="means")
-    public static void poisson(double mean) {
+    public static void poisson(double mean, long seed) {
         int nsp = 0;
         int nrp = 0;
-        final MersenneTwister random = new MersenneTwister();
+        final MersenneTwister random = new MersenneTwister(seed);
         final int ntrials = 100000;
 
         for (int i = 0; i < ntrials; i++)
             nrp += random.poisson(mean);
 
         double sigma = Math.sqrt(mean) * Math.sqrt(ntrials);
-        log.info("poisson: nevents mean={} expect={} nrp={} σ={}",
-                 mean, ntrials * mean, nrp, sigma);
+        log.info("poisson: mean={} σ={} expect={} actual={}({}σ)",
+                 mean, sigma, ntrials * mean, nrp, (ntrials * mean - nrp)/sigma);
         assertApproxEquals(nrp, ntrials * mean, 0, Math.max(10 * sigma, 1));
     }
 
-    @Test(dataProvider="means")
-    public static void gaussianStep(double mean) {
+    @Test(dataProvider="means", threadPoolSize = 4, invocationCount = 5)
+    public static void gaussianStep(double mean, long seed) {
         int nsp = 0;
-        final MersenneTwister random = new MersenneTwister();
+        final MersenneTwister random = new MersenneTwister(seed);
         final int ntrials = 100000;
 
         final int ntot = 1000;
@@ -65,8 +69,8 @@ public class TestStepGenerator {
             nsp += StepGenerator.gaussianStep(ntot, p, random.gaussian(), random.random());
 
         double sigma = Math.sqrt(mean * (ntrials - mean) / ntrials) * Math.sqrt(ntrials);
-        log.info("gaussian step: nevents mean={} expect={} nsp={} σ={}",
-                 mean, ntrials * mean, nsp, sigma);
+        log.info("gaussian step: mean={} σ={} expect={} actual={}({}σ)",
+                 mean, sigma, ntrials * mean, nsp, (ntrials*mean - nsp) / sigma);
         assertApproxEquals(nsp, ntrials * mean, 0, Math.max(10 * sigma, 1));
     }
 }
