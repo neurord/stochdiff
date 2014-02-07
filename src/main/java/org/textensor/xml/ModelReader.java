e04de5ccbfbc738384421b9700674b444dcd49a7
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2014-02-07 16:47:18 -0500

    Assume our namespace if nothing is specified

--- src/main/java/org/textensor/xml/ModelReader.java
+++ src/main/java/org/textensor/xml/ModelReader.java
@@ -10,6 +10,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.UnmarshallerHandler;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.sax.SAXSource;
@@ -19,15 +20,56 @@ import javax.xml.validation.Schema;
 import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBException;
 import org.xml.sax.InputSource;
+import org.xml.sax.Attributes;
 import org.xml.sax.XMLReader;
+import org.xml.sax.XMLFilter;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.XMLFilterImpl;
 
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import org.textensor.stochdiff.model.SDRun;
 
 public class ModelReader<T> {
+    static final Logger log = LogManager.getLogger(ModelReader.class);
+
+    public static final String STOCHDIFF_NS = "http://stochdiff.textensor.org";
+
+    public static class NamespaceFiller extends XMLFilterImpl {
+        boolean sdrun_seen = false;
+        boolean warning = false;
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes atts)
+            throws SAXException
+        {
+            if (!this.sdrun_seen && localName.equals("SDRun"))
+                this.sdrun_seen = true;
+            if (this.sdrun_seen && uri.equals("")) {
+                if (!this.warning) {
+                    this.warning = true;
+                    log.warn("Namespace not specified (seen at element {}), assuming {}",
+                             qName, STOCHDIFF_NS);
+                }
+                uri = STOCHDIFF_NS;
+            }
+            super.startElement(uri, localName, qName, atts);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName)
+            throws SAXException
+        {
+            if (this.sdrun_seen && uri.equals(""))
+                uri = STOCHDIFF_NS;
+            super.endElement(uri, localName, qName);
+        }
+    }
 
     JAXBContext jc;
 
@@ -42,6 +84,8 @@ public class ModelReader<T> {
     public T unmarshall(String filename)
         throws Exception
     {
+        log.info("umarshalling {}", filename);
+
         SAXParserFactory spf = SAXParserFactory.newInstance();
         spf.setXIncludeAware(true);
         spf.setNamespaceAware(true);
@@ -53,14 +97,24 @@ public class ModelReader<T> {
         Schema schema = factory.newSchema(schemaSource);
         spf.setSchema(schema);
 
+        XMLFilter filter = new NamespaceFiller();
         XMLReader xr = spf.newSAXParser().getXMLReader();
-        InputSource input = new InputSource(filename);
-        SAXSource source = new SAXSource(xr, input);
+        filter.setParent(xr);
+
+        InputSource xml = new InputSource(filename);
 
         Unmarshaller u = jc.createUnmarshaller();
+        UnmarshallerHandler uh = u.getUnmarshallerHandler();
         u.setSchema(schema);
         
-        return (T) u.unmarshal(source);
+        filter.setContentHandler(uh);
+        filter.parse(xml);
+
+        T result = (T) uh.getResult();
+        if (result == null)
+            throw new RuntimeException("Unmarshalling failed");
+
+        return result;
     }
 
     public T unmarshall(File filename)
@@ -68,8 +122,6 @@ public class ModelReader<T> {
     {
         return this.unmarshall(filename.toString());
     }
-
-    public static final String STOCHDIFF_NS = "http://stochdiff.textensor.org";
 
     public Marshaller getMarshaller(T object)
         throws Exception
