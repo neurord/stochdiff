08392582736b67b44b60f68b9e6378dd1672a560
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-03-18 11:17:57 -0400

    python: expose more properties of output

--- python/stochdiff/output.py
+++ python/stochdiff/output.py
@@ -22,20 +22,73 @@ i.e.
 from __future__ import print_function, division, unicode_literals
 
 import operator
+import enum
 import tables
 import numpy as np
 import pandas as pd
+from lxml import etree
 
 AVOGADRO = 6.02214179
 """Avogadro constant from CODATA 2006"""
 PUVC = AVOGADRO / 10
 """Converts concentrations to particle numbers"""
 
+class EventType(enum.IntEnum):
+    """Event types matching IGridCalc.EventType enumeration"""
+    REACTION = 0
+    DIFFUSION = 1
+    STIMULATION = 2
+
+class Dependencies(object):
+    """Raw information about the dependency graph
+
+    >>> out = Output('model.h5')
+    >>> deps = out.model.dependencies
+    >>> for t, e, d, dep in zip(deps.types(),
+    ...                         deps.elements(),
+    ...                         deps.descriptions(),
+    ...                         deps.dependent()):
+    ...     print('type {} in voxel {} "{}" dependent: {}'.format(t, e, d, dep))
+    type 0 in voxel 0 "NextReaction el.0 A→n" dependent: []
+    """
+    def __init__(self, element):
+        self._element = element
+
+    def indices(self):
+        "Numbers of the elements"
+        return range(self._element.descriptions.shape[0])
+
+    def descriptions(self):
+        "A generator of descriptions of nodes (by index)"
+        # pytables bug?
+        for row in self._element.descriptions[:]:
+            yield row.decode('utf-8')
+
+    def elements(self):
+        """The numbers of voxels events are attached to
+
+        In case of diffusion, those are the originating voxels.
+        """
+        return self._element.elements
+
+    def types(self):
+        # pytables bug?
+        for row in self._element.types[:]:
+            yield EventType(row)
+
+    def dependent(self):
+        "A generator of lists of dependent nodes (by index)"
+        # pytables bug?
+        for row in self._element.dependent[:]:
+            yield list(n for n in row if n >= 0)
+
+
 class Model(object):
     """Information about the model, same for all trials
     """
     def __init__(self, element):
-        self._model = element
+        self._element = element
+        self.dependencies = Dependencies(self._element.dependencies)
 
     def species(self):
         """List of specie names
@@ -47,7 +100,7 @@ class Model(object):
         >>> model.species
         ['A']
         """
-        return list(sp.decode('utf-8') for sp in self._model.species)
+        return list(sp.decode('utf-8') for sp in self._element.species)
 
     def grid(self):
         """Voxels of the simulation
@@ -64,7 +117,32 @@ class Model(object):
         array([ 2.])
 
         """
-        return self._model.grid.read().view(np.recarray)
+        return self._element.grid.read().view(np.recarray)
+
+    def element_regions(self):
+        "Names of regions of elements (by index)"
+        regions = np.array(self.region_names())
+        return regions[self.grid().region]
+
+    def indices(self):
+        "Numbers of the elements"
+        return range(self._element.species.shape[0])
+
+    def neighbors(self):
+        "A generator of lists of neighboring nodes (by index)"
+        # pytables bug?
+        for row in self._element.neighbors[:]:
+            yield list(n for n in row if n >= 0)
+
+    def couplings(self):
+        "A generator of coupling strengths to neighboring nodes (by index)"
+        coupl = self._element.couplings
+        for i, neigh in enumerate(self.neighbors()):
+            yield list(coupl[i][:len(neigh)])
+
+    def region_names(self):
+        "Region names (by index)"
+        return [row.decode('utf-8') for row in self._element.regions]
 
 class Simulation(object):
     """Information about the results of a trial
@@ -87,6 +165,30 @@ class Simulation(object):
         frame.index.names = ['voxel', 'time']
         return frame
 
+    def concentrations(self):
+        "Counts converted to concentrations using voxel volumes"
+        counts = self.counts()
+        volumes = self.model.grid().volume
+        ans = counts / volumes / PUVC
+        ans.rename(columns={'count':'concentration'}, inplace=1)
+        return ans
+
+    def config(self):
+        """lxml etree of de-serialized config the simulation was run with
+
+        Accessing serialized config
+
+        >>> out = Output('model.h5')
+        >>> xml = out.simulation(0).config()
+        >>> xml
+        <Element {http://stochdiff.textensor.org}SDRun at 0x...>
+        >>> xml.find('./ns:geometry', {'ns':'http://stochdiff.textensor.org'}).text
+        2D
+        """
+
+        xml = self._sim.serialized_config
+        return etree.fromstring(xml.read()[0])
+
 class Output(object):
     """The output for a single model, 0 or more experiments
 
@@ -95,6 +197,19 @@ class Output(object):
     def __init__(self, filename):
         self.file = tables.openFile(filename)
         self.model = Model(self.file.root.trial0.model)
+
+    def simulation(self, num):
+        """Get simulation by number
+
+        >>> out = Output('model.h5')
+        >>> sim = out.simulation(0)
+        >>> sim.number
+        0
+        >>> sim.config()
+        <Element {http://stochdiff.textensor.org}SDRun at 0x...>
+        """
+        trial = self.file.get_node('/trial{}'.format(num))
+        return Simulation(trial.simulation, self.model)
 
     def simulations(self):
         trials = self.file.list_nodes('/')
