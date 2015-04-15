63eb8d1d84073808c81ec70f918f0ecc4cb61d66
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-04-15 13:44:27 -0400

    python: add support for stacked histograms

--- python/stochdiff/ks.py
+++ python/stochdiff/ks.py
@@ -1,39 +1,47 @@
 import numpy as np
 import pandas as pd
 
-def bincount_axis1(data):
+def bincount_axis1(data, max, weights=None):
     """Count the number of occurences of each value.
 
     Returns an array of counts corresponding to bins
     [0-1), [1-2), [max-1, max]. The right bin is closed on both
     sides, unlike all others!
     """
-    max = data.max()
-    ans = [np.bincount(col, minlength=max+1)[:, None]
-           for col in data.T]
+    if weights is None:
+        ans = [np.bincount(col, minlength=max+1)[:, None]
+               for col in data.T]
+    else:
+        ans = [np.bincount(col, wei, minlength=max+1)[:, None]
+               for col, wei in zip(data.T, weights.T)]
     ans = np.hstack(ans)
     ans[-2] += ans[-1]
     return ans[:-1]
 
-def histogram(items, normed=False, cumulative=False, bins=None):
+def histogram(items, weights=None, normed=False, cumulative=False, min_max=None, bins=None):
     """Compute normed cdf of distribution of counts"""
-    left = items.min()
-    right = items.max()
+    if min_max is None:
+        left = items.min()
+        right = items.max()
+    else:
+        left, right = min_max
     binsize = (right - left) / bins if bins is not None else 1
     where = np.empty_like(items, dtype=int)
     np.floor_divide(items - left, binsize, out=where)
-    hist = bincount_axis1(where)
+    hist = bincount_axis1(where, (right-left)/binsize, weights)
     hist = np.vstack((0*hist[0], hist, 0*hist[0]))
     if normed:
         out = np.empty_like(hist, dtype=float)
         hist = np.true_divide(hist, items.shape[0], out=out)
     if cumulative:
         hist = hist.cumsum(axis=0)
-    x = np.arange(-0.5, hist.shape[0]-0.5) * binsize + left
+    x = np.arange(0, hist.shape[0]) * binsize + left
     return x, hist
 
-def apply_histogram(items, normed=False, cumulative=False, bins=None):
-    x, hist = histogram(items.values, normed=normed, cumulative=cumulative, bins=bins)
+def apply_histogram(items, weights=None, normed=False, cumulative=False, min_max=None, bins=None):
+    x, hist = histogram(items.values,
+                        weights.values if weights is not None else None,
+                        normed=normed, cumulative=cumulative, min_max=min_max, bins=bins)
     if normed:
         label = 'frequency'
     else:
