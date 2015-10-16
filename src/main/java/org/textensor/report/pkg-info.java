50098566ff0b7f021fe1f9c3c9bb72264eb04f97
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-10-16 14:45:34 -0400

    Remove unused class

--- src/main/java/org/textensor/report/pkg-info.java
+++
@@ -1,9 +0,0 @@
-/**
- *
- * This is mainly for debugging.
- *
- * <p>
- * Static methods in class E generate output in the right format to be picked up by Eclipse and
- * converted to links back to the relevant point in the code.
- *
- */
