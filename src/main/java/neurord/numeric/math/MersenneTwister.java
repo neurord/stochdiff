neurord/stochdiff

commit 0b335962c919dc407b842b0eef26985f9de2d4c7

Move randomRound to Random

src/main/java/neurord/numeric/math/MersenneTwister.java
@@ -201,6 +201,14 @@ public class MersenneTwister implements RandomGenerator {
             return - Math.log1p(-random()) / tau;
     }
 
+    @Override
+    public final int round(double mean) {
+        int n = (int) mean;
+        if (this.random() > mean - n)
+            n++;
+        return n;
+    }
+
     public static void main(String[] argv) {
 
         MersenneTwister mt = new MersenneTwister();

src/main/java/neurord/numeric/grid/SteppedStochasticGridCalc.java
@@ -205,7 +205,7 @@ public class SteppedStochasticGridCalc extends StochasticGridCalc {
                         double as = astim[j] / nk;
 
                         for (int k = 0; k < nk; k++) {
-                            int nin = this.randomRound(as);
+                            int nin = this.random.round(as);
                             int tgt = stimtargets[i][k];
                             wkA[tgt][j] += nin;
                             if (this.stimulationEvents != null)

src/main/java/neurord/numeric/grid/StochasticGridCalc.java
@@ -58,7 +58,7 @@ public abstract class StochasticGridCalc extends GridCalc {
             double[] rcs = sdrun.getRegionConcentrations()[eltregions[i]];
 
             for (int j = 0; j < nspec; j++)
-                wkA[i][j] = this.randomRound(v * rcs[j] * PARTICLES_PUVC);
+                wkA[i][j] = this.random.round(v * rcs[j] * PARTICLES_PUVC);
         }
 
         /* surface concentrations */
@@ -72,7 +72,7 @@ public abstract class StochasticGridCalc extends GridCalc {
                 for (int j = 0; j < nspec; j++)
                     if (!Double.isNaN(scs[j]))
                         // nan means not specified by the user;
-                        wkA[i][j] = this.randomRound(a * scs[j] * PARTICLES_PUASD);
+                        wkA[i][j] = this.random.round(a * scs[j] * PARTICLES_PUASD);
         }
 
 
@@ -81,18 +81,6 @@ public abstract class StochasticGridCalc extends GridCalc {
     @Override
     protected void footer() {
         log.info("Used up {} random numbers", this.random.used());
-    }
-
-    protected int randomRound(double number) {
-        int i = (int) number;
-        double d = number - i;
-
-        // random allocation to implement the remainder
-        // (some cells get an extra particle, some don't)
-        if (random.random() < d)
-            i++;
-
-        return i;
     }
 
     @Override

src/main/java/neurord/numeric/math/RandomGenerator.java
@@ -7,6 +7,11 @@ public interface RandomGenerator {
     int poisson(double mean);
 
     /**
+     * Randomly round up or down, so the average is @mean.
+     */
+    int round(double mean);
+
+    /**
      * A number from the probability distribution
      *    [ t ≥ 0] a exp(-at)
      */

commit d7f93baf7d677447a41675e30557dbc492079d8b

Fix rounding to the closest integer
It was rounding to the further integer instead :(
Bug introduced in 0b335962c919.

src/main/java/neurord/numeric/math/MersenneTwister.java
@@ -204,7 +204,7 @@ public class MersenneTwister implements RandomGenerator {
     @Override
     public final int round(double mean) {
         int n = (int) mean;
-        if (this.random() > mean - n)
+        if (this.random() < mean - n)
             n++;
         return n;
     }