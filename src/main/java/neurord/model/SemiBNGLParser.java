4c8eb2dd0268354b5a95d6a8c9d2e82bc6a63ceb
Alireza Shakerpoor <alireza.shakerpoor@gmail.com>
2024-07-18 16:18:34 -0500

    Add method to partse parameters

--- src/main/java/neurord/model/SemiBNGLParser.java
+++ src/main/java/neurord/model/SemiBNGLParser.java
@@ -10,11 +10,11 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class SemiBNGLParser {
-	private Map<String, String> parameters;
+	private Map<String, Integer> parameters;
 	private Map<Species, Integer> seedSpecies;
 	private List<Molecule> moleculeTypes;
-	private List<Pattern> observables;
-	private List<String> reactionRules;
+	private List<Pattern> observables; // Using a generic type instead?
+	private List<String> reactionRules; // Using a list of objects instead?
 	
 	public SemiBNGLParser() {
 		parameters = new HashMap<>();
@@ -45,7 +45,8 @@ public class SemiBNGLParser {
 			}
 		}
 	}
-	
+
+/*
 	private void parseParameters(BufferedReader reader) throws ParseException {
 		String line;
 		while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end parameters")) {
@@ -53,7 +54,7 @@ public class SemiBNGLParser {
 			if (param.length == 2)
 				parameters.put(param[0], param[1]);
 			else if (param.length > 2) {
-				// TODO: Add support for special math operators and chars ^()*/+-e
+				// TODO: Add support for special math operators and chars ^()*,/+-e
 				// Handle multi-word parameter values separated by white space
 				String key = param[0];
 				StringBuilder value = new StringBuilder(param[1]);
@@ -64,46 +65,125 @@ public class SemiBNGLParser {
 			}
 		}
 	}
+*/
+
+    private void parseParameters(BufferedReader reader) throws IOException {
+    	String line;
+    	try {
+    		while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end parameters")) {
+    			String[] param =  line.split("\\s+");
+    			if (param.length == 2) {
+    				Integer value = constructPValueFromLine(param[1]);
+    				parameters.put(param[0], value.intValue());
+    			} else if (param.length > 2) {
+    				// Handle multi-word parameter values separated by white space
+    				StringBuilder sb = new StringBuilder(param[1]);
+    				for (int i = 2; i < param.length; i++)
+    					sb.append(" ").append(param[i]);
+    				
+    				// TODO: Add support for special math operators and chars ^()*,/+-e
+    				Integer value = constructPValueFromLine(sb.toString());
+    				parameters.put(param[0], value.intValue());
+    			}
+    		}
+    	} catch (IOException e) {
+    		throw new IOException("Error reading parameters", e);
+    	}
+    }
 	
-	private void parseMoleculeTypes(BufferedReader reader) throws ParseException {
+	private void parseMoleculeTypes(BufferedReader reader) throws IOException {
 		String line;
-		while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end molecule types")) {
-			// Add the logic that constructs a molecule and adds it to moleculeTypes
-			
+		try {
+			while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end molecule types")) {
+				Molecule molecule = new  constructMoleculeFromLine(line);
+				moleculeTypes.add(molecule);
+			}
+		} catch (IOException e) {
+			throw new IOException("Error reading molecule types", e);
 		}
 	}
 	
-	private void parseSeedSpecies(BufferedReader reader) throws ParseException {
+	private void parseSeedSpecies(BufferedReader reader) throws IOException {
 		String line;
-		while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end seed species")) {
-			String[] param = line.split("\\s+");
-//			if (param.length == 2)
-				// Add the logic to construct Species and Integer objects
-				// from the line input.
-//				seedSpecies.put(param[0], param[1]);
-//			else if (param.length > 2) {
-//				// TODO: Add support for special math operators and chars ^()*/+-e
-//				// Handle multi-word parameter values separated by white space
-//				String key = param[0];
-//				StringBuilder value = new StringBuilder(param[1]);
-//				for (int i = 2; i < param.length; i++)
-//					value.append(param[i]);
-//				
-//				seedSpecies.put(key, value.toString());
-//			}
+		try {
+			while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end seed species")) {
+				Species species = new constructSpeciesFromLine(line);
+				Integer seed = new constructSeedFromLine(line);
+				seedSpecies.put(species, seed.intValue());
+			}
+		} catch (IOException e) {
+			throw new IOException("Error reading seed species", e);
 		}
 	}
 	
-	private void parseObservables(BufferedReader reader) throws ParseException {
-		
+	private void parseObservables(BufferedReader reader) throws IOException {
+		String line;
+		try {
+			while (!(line = reader.readLine().replaceAll("^\\s*#.*$", "").trim()).equals("end observables")) {
+				// Some logic here depending on the types declared
+			}
+		} catch (IOException e) {
+			throw new IOException("Error reading observables", e);
+		}
 	}
 	
-	private void parseReactionRules(BufferedReader reader) throws ParseException {
-		
+	private void parseReactionRules(BufferedReader reader) throws IOException {
+		// Some logic here depending on the types declared
 	}
+	
+	private Integer constructPValueFromLine(String pValue) {
+		// parameter evaluator 
+		// parameterExpr
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	// Getters for the parsed data
-	public HashMap<String, String> getParameters() {
+	public HashMap<String, Integer> getParameters() {
 		return parameters;
 	}
 	
@@ -112,15 +192,15 @@ public class SemiBNGLParser {
 	}
 	
 	public List<Molecule> getMoleculeTypes() {
-		
+		return moleculeTypes;
 	}
 	
 	public List<Pattern> getObservables () {
-		
+		return observables;
 	}
 	
 	public List<String> getReactionRules () {
-		
+		return reactionRules;
 	}
 	
 	
