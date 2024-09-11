15a9236e4237e54a2c97fc8ca8fce369a56681ea
Alireza Shakerpoor <alireza.shakerpoor@gmail.com>
2024-09-10 19:41:25 -0500

    ReactionRule: Fix bug in the class to handle null moleculeTypes list

--- src/main/java/neurord/model/ReactionRule.java
+++ src/main/java/neurord/model/ReactionRule.java
@@ -35,39 +35,29 @@ public class ReactionRule {
 	private void parseReaction() {
 		if (reaction.length != 2)
 			throw new IllegalArgumentException("Invalid reaction format.");
-			
-		parseReactants(reaction[0]);
-		parseProducts(reaction[1]);
+
+		parseReactionUnits(reaction[0],0);
+		parseReactionUnits(reaction[1],1);
 	}
 	
-	private void parseReactants(String reactants) {
+	private void parseReactionUnits(String units, int index) {
 		// Regular expression to match components of the form R1(...) and R1(...).R2(...)
 		String regex = "\\w+\\([^\\)]+\\)(?:\\.\\w+\\([^\\)]+\\))*";
 		// create a regular Pattern object (from java's util)
 		java.util.regex.Pattern regPattern = java.util.regex.Pattern.compile(regex);
-		Matcher matcher = regPattern.matcher(reactants);
+		Matcher matcher = regPattern.matcher(units);
 		// Find all matches and create the corresponding Pattern object (from neurord.model)
 		while (matcher.find()) {
-			Pattern reactant;
-			if (moleculeTypes.isEmpty())
-				reactant = new Pattern(matcher.group());
+			Pattern unit;
+			if (moleculeTypes == null)
+				unit = new Pattern(matcher.group());
 			else
-				reactant = new Pattern(matcher.group(), moleculeTypes);
-			this.reactants.add(reactant);
-		}	
-	}
-	
-	private void parseProducts(String products) {
-		String regex = "\\w+\\([^\\)]+\\)(?:\\.\\w+\\([^\\)]+\\))*";
-		java.util.regex.Pattern regPattern = java.util.regex.Pattern.compile(regex);
-		Matcher matcher = regPattern.matcher(products);
-		while (matcher.find()) {
-			Pattern product;
-			if (moleculeTypes.isEmpty())
-				product = new Pattern(matcher.group());
+				unit = new Pattern(matcher.group(), moleculeTypes);
+			
+			if (index == 0)
+				this.reactants.add(unit);
 			else
-				product = new Pattern(matcher.group(),moleculeTypes);
-			this.products.add(product);
+				this.products.add(unit);
 		}
 	}
 	
